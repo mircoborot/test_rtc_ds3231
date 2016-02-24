958e9f7c262115d9e8f04c1f714a7f07faa9e02c
mircoborot <antonio.curci.it@gmail.com>
2016-02-25 00:00:37 +0100

    Added files via upload

---
+++ test_rtc_ds3231_v01r01/rtc.ino
@@ -0,0 +1,73 @@
+/* ---------------------------------------------------------
+Package     :  wsn v.1
+Module      :  rtc
+Copyright   :  (c) 2016 by Antonio Curci 
+
+Copyright 2014-2016 by Antonio Curci. All rights reserved.
+Redistribution and use in source and binary forms,
+with or without modification, are not permitted.
+-------------------------------------------------------- */
+
+// ******************** WARNINGS
+/* 
+ Note: 
+  1. temperature is updated every 64 seconds by the DS3231
+  2. DateTime struct:
+     Second: 0-59 
+     Minute: 0-59
+     Hour:   0-23
+     Dow:    1-7  (1=Sunday)
+     Day:    1-31
+     Month:  1-12
+     Year:   0-199
+
+*/
+
+// ******************** LIBRARIES
+#include <DS3231_Simple.h>
+
+// ******************** DEFINES
+
+// ******************** CONSTANTS
+
+// ******************** VARIABLES
+boolean rtn;    // enabled
+
+DS3231_Simple Clock;
+DateTime dt;  
+
+// ******************** FUNCTIONS
+void rtcsetup() {Clock.begin();rtcen(true);}
+void rtcen(bool f) {rtn=f;}  
+bool rtcena()  {return rtn;}  
+
+void rtcread() {dt=Clock.read();}
+void rtcwrite() {Clock.write(dt);}
+
+float rtcintemp() {return Clock.getTemperatureFloat();}
+
+// get
+byte rtcinYY() {return dt.Year;}
+byte rtcinMM() {return dt.Month;}
+byte rtcinDD() {return dt.Day;}
+byte rtcinDW() {return dt.Dow;}
+byte rtcinhh() {return dt.Hour;}
+byte rtcinmm() {return dt.Minute;}
+byte rtcinss() {return dt.Second;}
+
+// set
+void rtcoutYY(byte x) {dt.Year=x;}
+void rtcoutMM(byte x) {dt.Month=x;}
+void rtcoutDD(byte x) {dt.Day=x;}
+void rtcoutDW(byte x) {dt.Dow=x;}
+void rtcouthh(byte x) {dt.Hour=x;}
+void rtcoutmm(byte x) {dt.Minute=x;}
+void rtcoutss(byte x) {dt.Second=x;}
+
+// print
+void rtcprdate() {Clock.printDateTo_DMY(Serial,dt);}  
+void rtcprdateYMD() {Clock.printDateTo_YMD(Serial,dt);}  
+
+void rtcprtime() {Clock.printTimeTo_HMS(Serial,dt);}  
+void rtcprtimeHM() {Clock.printTimeTo_HM(Serial,dt);}  
+
---
+++ test_rtc_ds3231_v01r01/test_rtc_ds3231_v01r01.ino
@@ -0,0 +1,59 @@
+/* ---------------------------------------------------------
+Package     :  wsn v.1
+Module      :  main
+Copyright   :  (c) 2016 by Antonio Curci 
+
+Copyright 2014-2016 by Antonio Curci. All rights reserved.
+Redistribution and use in source and binary forms,
+with or without modification, are not permitted.
+
+
+-------------------------------------------------------- */
+// ******************** DEFINES
+#define sersetup()  Serial.begin(9600)
+#define pr(x)  Serial.print(x)
+#define prln(x)  Serial.println(x)
+#define prf(x)  Serial.print(F(x))
+#define prfln(x)  Serial.println(F(x))
+
+// ******************** WARNINGS
+
+
+// ******************** CONSTANTS
+// ******************** VARIABLES
+// ******************** FUNCTIONS
+
+// ******************** ARDUINO
+void setup() {
+  sersetup();
+  rtcsetup();
+  prf("\r\ntest RTC DS3231 v.01.01 (C) Copyright 2016 by Antonio Curci\r\n");
+
+  rtcoutYY(16);
+  rtcoutMM(2);
+  rtcoutDD(24);
+  rtcoutDW(4);
+  rtcouthh(22);
+  rtcoutmm(33);
+  rtcoutss(00);
+  rtcwrite();
+}
+
+void loop() {
+  rtcread();
+  //rtcpr();
+  rtcprdate(); pr(' '); rtcprtime();
+  /*
+  prf("Hour: ");   prln(rtcinhh());
+  prf("Minute: "); prln(rtcinmm());
+  prf("Second: "); prln(rtcinss());
+  prf("Year: ");   prln(rtcinYY());
+  prf("Month: ");  prln(rtcinMM());
+  prf("Day: ");    prln(rtcinDD());
+  */
+  prf("  Dow: ");    pr(rtcinDW());
+  prf("  Temp: ");   prln(rtcintemp());
+
+  prln();
+  delay(3000);
+}
